04b959addbfe0624c5c7b20c6bbc09a5eabe404e
PavelShcherbyna <shcherbyna.pavel@gmail.com>
2023-12-14 16:03:24 +0100

    Added pwa popup event listener and button to trigger this popup
    Took 1 hour 33 minutes

--- src/components/GeneratorMainBlock/GeneratorMainBlock.jsx
+++ src/components/GeneratorMainBlock/GeneratorMainBlock.jsx
@@ -18,6 +18,7 @@ import ArrowsNavigation from '../ArrowsNavigation';
 import { removeSessionStorageItem } from '../../helpers/sessionStorage';
 import { access_token } from '../../constants';
 import { NoSleepContext } from '../../App';
+import { ButtonWithBorder } from '../reusableStyles';
 
 const GeneratorMainBlock = () => {
   const [baseImageSrc, setBaseImageSrc] = useState('');
@@ -33,6 +34,7 @@ const GeneratorMainBlock = () => {
   const [pickStepModalOpen, setPickStepModalOpen] = useState(false);
   const baseImgRef = useRef(null);
   const [lineCalcProgress, setLineCalcProgress] = useState(0);
+  const [showInstallBtn, setShowInstallBtn] = useState(false);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -53,6 +55,32 @@ const GeneratorMainBlock = () => {
   // const generalSettingsArr = lineSetArr.map((linesNum, index) => {
   //   return { lines: Number(linesNum), outputCanvasId: `canvasOutput${index}` };
   // });
+
+  async function installAppHandler() {
+    console.log('👍', 'butInstall-clicked');
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      // The deferred prompt isn't available.
+      return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log('👍', 'userChoice', result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    setShowInstallBtn(false);
+    // divInstall.classList.toggle('hidden', true);
+  }
+
+  useEffect(() => {
+    if (!!window.deferredPrompt) {
+      setShowInstallBtn(true);
+    }
+  }, []);
 
   useEffect(() => {
     if (justGenDrawId) {
@@ -257,6 +285,11 @@ const GeneratorMainBlock = () => {
 
   return (
     <>
+      {showInstallBtn && (
+        <ButtonWithBorder onClick={installAppHandler}>
+          Установить на рабочий стол?
+        </ButtonWithBorder>
+      )}
       {/*<img className="hidden" id="imageSrc" alt="Source" src={baseImageSrc} />*/}
       {!baseImageSrc && generatorStep === 0 && (
         <>
